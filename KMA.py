91edf6a4729a5b14b2509d44e0a85def0dbd67f5
chaseungjoon <cktmdwns3@gmail.com>
2025-08-09 20:12:10 +0900

    Reformat KMA.py and add HSR_comp data

--- KMA.py
+++ KMA.py
@@ -11,25 +11,28 @@ def get_data(choice):
     if choice == 1:
         url = f"{base_url}/typ01/url/kma_sfctm2.php?stn=0&dataType=JSON&help=1&authKey={auth}"
         file_type = "json"
+        data_type = "ASOS"
     # 대기 정보 binary
     elif choice == 2:
         url = f"{base_url}/typ04/url/rdr_cmp_file.php?&data=bin&cmp=cpp&authKey={auth}"
         file_type = "bin"
+        data_type = "HSR_bin"
     # 대기 정보 image (인코딩 필요)
     elif choice == 3:
         url = f"{base_url}/typ04/url/rdr_cmp_file.php?&data=img&cmp=cmb&authKey={auth}"
         file_type = "cmb"
+        data_type = "HSR_img"
     # 향후 1시간 일기예보 (5분마다 업데이트)
     elif choice == 4:
         url = f"{base_url}/typ01/cgi-bin/url/nph-rdr_cmp_inf?&cmp=HSR&qcd=MSK&disp=A&authKey={auth}"
         file_type = "txt"
+        data_type = "HSR_comp"
     else:
-        url = None
-        file_type = None
+        print("\nInvalid choice\n")
+        return
 
-    """ Fetch data"""
     timestamp = time.strftime("%y%m%d%H%M%S")
-    save_path = os.path.join(os.getcwd(), "Data", f"{timestamp}.{file_type}")
+    save_path = os.path.join(os.getcwd(), "Data", f"{data_type}_{timestamp}.{file_type}")
     response = requests.get(url)
     response.encoding = 'euc-kr'
     with open(save_path, "w", encoding="utf-8") as f:
@@ -41,4 +44,3 @@ if __name__ == "__main__":
         if option == 0:
             break
         get_data(option)
-
