eed57e7e4314f2203f617f56b74d458f24c58944
chaseungjoon <cktmdwns3@gmail.com>
2025-08-19 15:04:04 +0900

    Add GK2A(KMA) endpoint to config.py

--- config.py
+++ config.py
@@ -5,10 +5,12 @@ KMA_WEATHER_TOKEN = os.getenv("KMA_WEATHER_TOKEN")
 NASA_FIRMS_MAP_KEY = os.getenv("NASA_FIRMS_MAP_KEY")
 
 """ directories """
+DOWNLOAD_DEFAULT_DIR = "/Users/chaseungjun/Downloads"
 ROOT_DIR = os.getcwd()
 FIRE_DATA_DIR = os.path.join(ROOT_DIR, "fire_data")
 MAP_DATA_DIR = os.path.join(ROOT_DIR, "map_data")
 WEATHER_DATA_DIR = os.path.join(ROOT_DIR, "weather_data")
+SATELLITE_DATA_DIR = os.path.join(ROOT_DIR, "satellite_data")
 
 """ files """
 GRID_PATH = os.path.join(MAP_DATA_DIR, "grid.csv")
@@ -21,6 +23,7 @@ KMA_ENDPOINTS = {
         3: {"url": KMA_AWS_BASE_URL + f"_lst?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS_temp", "filetype":"csv", "desc": "AWS 초상온도"},
         4: {"url": KMA_AWS_BASE_URL + f"_vis?authKey={KMA_WEATHER_TOKEN}", "filename": "AWS_vis", "filetype":"csv", "desc": "AWS 가시거리"},
 }
+KMA_SATELLITE_BASE_URL = "https://apihub.kma.go.kr/api/typ05/api/GK2A/LE2/FF/KO/data?date="
 
 """ nasa firms api """
 NASA_FIRMS_URL = "https://firms.modaps.eosdis.nasa.gov/api/area/csv/"+NASA_FIRMS_MAP_KEY+"/VIIRS_NOAA20_NRT/124.5,33.0,131.0,39.5"
