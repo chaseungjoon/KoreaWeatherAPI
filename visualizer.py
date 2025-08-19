ea41b3fe54336148bb1c2d3b33d9cd1114e00ed4
chaseungjoon <cktmdwns3@gmail.com>
2025-08-19 15:07:49 +0900

    Add visual map for GK2A fire data

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,6 @@
 import os
 import folium
-from csv_reader import load_grid, load_fire_grid
+from csv_reader import load_grid, load_nasa_fire_grid, load_GK2A_fire_grid
 from config import MAP_DATA_DIR
 
 def draw_grid():
@@ -21,10 +21,28 @@ def draw_grid():
 
     m.save(save_path)
 
-def draw_fire_grid():
+def draw_GK2A_fire_grid():
+    save_path = os.path.join(MAP_DATA_DIR, "fire_map_GK2A.html")
+
+    grid = load_GK2A_fire_grid()
+    center_lat = sum(d['LAT'] for d in grid) / len(grid)
+    center_lon = sum(d['LON'] for d in grid) / len(grid)
+
+    m = folium.Map(location=[center_lat, center_lon], zoom_start=7)
+
+    for d in grid:
+        folium.Marker(
+            location=[d['LAT'], d['LON']],
+            popup=f"DQF_FF = {d['DQF_FF']}",
+            icon = folium.Icon(color="red", icon="info-sign")
+        ).add_to(m)
+
+    m.save(save_path)
+
+def draw_nasa_fire_grid():
     save_path = os.path.join(MAP_DATA_DIR, "fire_map.html")
 
-    grid = load_fire_grid()
+    grid = load_nasa_fire_grid()
     center_lat = sum(d['LAT'] for d in grid) / len(grid)
     center_lon = sum(d['LON'] for d in grid) / len(grid)
 
